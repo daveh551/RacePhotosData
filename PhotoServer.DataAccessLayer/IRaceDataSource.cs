cdafeabaa19e3790754736367d0f88d8162ffd06
Dave Hanna <dave@infotouchsys.com>
2013-06-03 05:32:15 -0500

    Add RaceDataSource

---
+++ PhotoServer.DataAccessLayer/IRaceDataSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhotoServer.Domain;
+
+namespace PhotoServer.DataAccessLayer
+{
+	public interface IRaceDataSource
+	{
+		IRepository<Race, int> Races { get; }
+		IRepository<Event, int> Events { get; }
+		IRepository<Distance, int> Distances { get;  } 
+	}
+}
